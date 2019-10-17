f18ade2542812a8a0b58e9eed0dc76532df66bfe
Tate Stone <tatejamesstone@gmail.com>
2019-10-17 14:33:21 -0600

    Work on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,13 @@ function Header() {
   return (
     <div className="header">
         <img clasName='logo' src={logo} alt="logo"/>
-        
+        <div className="options">
+            <p>Main</p>
+            <p>About</p>
+            <p>Projects</p>
+            <p>Stack</p>
+            <p>Contact</p>
+        </div>
     </div>
   );
 }
