4880d20933eaf2c951392f0ba1bfaa58671b4b82
Tate Stone <tatejamesstone@gmail.com>
2019-10-17 13:52:49 -0600

    More work

---
+++ src/components/OtherPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './OtherPage.scss'
+
+function OtherPage() {
+  return (
+    <div className="otherpage">
+      OtherPage
+    </div>
+  );
+}
+
+export default OtherPage;
