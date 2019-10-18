0844aac0e42c8cdd14ff7e4e9a63238d3296035c
Tate Stone <tatejamesstone@gmail.com>
2019-10-18 13:43:27 -0600

    More work on styling the skills

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,10 +1,62 @@
 import React from 'react';
 import './Stack.scss'
+import css3 from '../assets/css3.png'
+import html5 from '../assets/html5.png'
+import sass from '../assets/sass.png'
+import javascript from '../assets/javascript.png'
+import reactjs from '../assets/reactjs.png'
+import nodejs from '../assets/nodejs.png'
+import postgressql from '../assets/postgressql.png'
+import gitpic from '../assets/git.png'
+import salesforce from '../assets/salesforce.png'
+import zuora from '../assets/zuora.jpg'
 
 function Stack() {
   return (
     <div className="stack">
       <h1>Skills</h1>
+      <div className="images">
+        <div className="container">
+          <img className='css3' src={css3} alt="css3"/>
+          <p>CSS3</p>
+        </div>
+        <div className="container">
+          <img className='html5' src={html5} alt="html5"/>
+          <p>HTML5</p>
+        </div>
+        <div className="container">
+          <img className='sass' src={sass} alt="sass"/>
+          <p>Sass</p>
+        </div>
+        <div className="container">
+          <img className='javascript' src={javascript} alt="javascript"/>
+          <p>Javascript</p>
+        </div>
+        <div className="container">
+          <img className='reactjs' src={reactjs} alt="reactjs"/>
+          <p>React</p>
+        </div>
+        <div className="container">
+          <img className='nodejs' src={nodejs} alt="nodejs"/>
+          <p>Node.js</p>
+        </div>
+        <div className="container">
+          <img className='postgressql' src={postgressql} alt="postgressql"/>
+          <p>PostgresSQL</p>
+        </div>
+        <div className="container">
+          <img className='gitpic' src={gitpic} alt="gitpic"/>
+          <p>Git</p>
+        </div>
+        <div className="container">
+          <img className='salesforce' src={salesforce} alt="salesforce"/>
+          <p>Salesforce</p>
+        </div>
+        <div className="container">
+          <img className='zuora' src={zuora} alt="zuora"/>
+          <p>Zuora</p>
+        </div>
+      </div>
     </div>
   );
 }
