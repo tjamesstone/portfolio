45da9d240f59e333d83f414612b88371887feaf0
Tate Stone <tatejamesstone@gmail.com>
2019-10-18 11:43:27 -0600

    Work on the picture animation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,9 @@ function Main() {
   return (
     <div className="main">
         <h1>Tate J Stone</h1>
-        
+        <div className="picture">
+          
+        </div>
     </div>  
   );
 }
