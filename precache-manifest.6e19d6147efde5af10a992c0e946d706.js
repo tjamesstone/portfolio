b27ffce8cbfda16d747aa54b6fe7ed41bec9a1d7
Tate Stone <tatejamesstone@gmail.com>
2019-10-21 16:36:34 -0600

    Updates

---
+++ precache-manifest.6e19d6147efde5af10a992c0e946d706.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f26d27d1dc9f8f5db5e281b94bdfbfb3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dbf8abf4a243e402c974",
+    "url": "/portfolio/static/css/main.c10fb10c.chunk.css"
+  },
+  {
+    "revision": "6e83d9202a1f645b11b1",
+    "url": "/portfolio/static/js/2.e5d195f7.chunk.js"
+  },
+  {
+    "revision": "dbf8abf4a243e402c974",
+    "url": "/portfolio/static/js/main.3df1af6f.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "1a9a9c54fadbdd845398dba0ecb5c26d",
+    "url": "/portfolio/static/media/1.1a9a9c54.png"
+  },
+  {
+    "revision": "95e85c67c0f1e1b9a8736389126d994e",
+    "url": "/portfolio/static/media/3.95e85c67.png"
+  },
+  {
+    "revision": "88b4a9bc67aa65b904c826994d791470",
+    "url": "/portfolio/static/media/4.88b4a9bc.png"
+  },
+  {
+    "revision": "a08c615948356d32649a535fdab2bf93",
+    "url": "/portfolio/static/media/5.a08c6159.png"
+  },
+  {
+    "revision": "27d1b96481625f9aafea622fae826ca6",
+    "url": "/portfolio/static/media/7.27d1b964.png"
+  },
+  {
+    "revision": "4cf00bda865a0838149ed7fcc67c235a",
+    "url": "/portfolio/static/media/byu2.4cf00bda.png"
+  },
+  {
+    "revision": "d04c9b6c7d3389c9baacd855d2a13b8a",
+    "url": "/portfolio/static/media/createacontract.d04c9b6c.png"
+  },
+  {
+    "revision": "17a6652f345bc5ea2db2d4fba0c95a84",
+    "url": "/portfolio/static/media/img_326384.17a6652f.png"
+  },
+  {
+    "revision": "425c0312ff1a933f3faa7539bef999cf",
+    "url": "/portfolio/static/media/linkedin.425c0312.png"
+  },
+  {
+    "revision": "74c362907475d0d7f0d59d0fb662135c",
+    "url": "/portfolio/static/media/sass.74c36290.png"
+  },
+  {
+    "revision": "70f54f4b2a8f7a6c30a2e1a3f847fc10",
+    "url": "/portfolio/static/media/sf2.70f54f4b.png"
+  },
+  {
+    "revision": "3abb83d9f82697985b162d4b859bb4af",
+    "url": "/portfolio/static/media/z.3abb83d9.png"
+  }
+]);
