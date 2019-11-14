f749aeb96ffa44ee2bf9fb0859407f5e80b45dca
Tate Stone <tatestone@Tates-MacBook-Pro.local>
2019-11-14 09:00:40 -0700

    Pretty much done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Projects.scss'
 import createacontract from '../assets/createacontract.png'
+import smakd from '../assets/smakd.png'
 
 function Projects() {
   return (
@@ -8,14 +9,14 @@ function Projects() {
       <h1>Projects</h1>
       <div className="theprojects">
         <div className="project1">
-          <a href="https://create-a-contract.com"><img src={createacontract} alt="project1"/></a>
+          <a href="https://create-a-contract.com"><img className='theultimateimage' src={createacontract} alt="project1"/></a>
           <a href="https://create-a-contract.com">            <h6>Create A Contract</h6></a>
             <p>Create A Contract allows you to create legally-binding template contracts for your company and then save them as a PDF. </p>
         </div>
-        <div className="project2">
-            <img src={createacontract} alt="project2"/>
-            <h6>Group Project</h6>
-            <p>This group project is awesome and you should take a look, I did great work and I'm proud of it too. thanks for looking</p>
+        <div className="project1">
+        <a href="https://www.smakd.fun/#/"><img className='theultimateimage' src={smakd} alt="project1"/></a>
+        <a href="https://www.smakd.fun/#/">            <h6>SMAK'D</h6></a>
+            <p>SMAK'D is a multiplayer party game built in React and heavily dependent upon sockets. This game can be played on your phone or on your computer. Check it out.</p>
         </div>
       </div>
     </div>
