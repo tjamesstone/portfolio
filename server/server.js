77fb0fb69af93a23d81d2b20cb307bd7b056df38
Tate Stone <tatestone@Tates-MacBook-Pro.local>
2019-11-19 16:21:38 -0700

    Fixed a line

--- server/server.js
+++ server/server.js
@@ -3,8 +3,7 @@ const express = require("express")
 const app = express()
 
 app.use(express.json())
-app.use(express.static(__dirname + '/../public/build'))
-
+app.use( express.static( `${__dirname}/../build` ) );
 
 let port = 4444;
 app.listen(port, () => {
