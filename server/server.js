13554967d92ddc48fe48188c87c06c1187db77e5
Tate Stone <tatestone@Tates-MacBook-Pro.local>
2019-11-19 16:46:34 -0700

    More work

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require("express")
+const path = require('path'); // Usually moved to the start of file
+
 
 const app = express()
 
+
 app.use(express.json())
 app.use( express.static( `${__dirname}/../build` ) );
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 let port = 4444;
 app.listen(port, () => {
